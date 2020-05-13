b105f7ba6f81cf7c13432b8de37572cf8de15be6
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-05-13 17:00:05 +0200

    Aggiungere i file di progetto.

---
+++ Collezioni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Libreria;
+
+namespace Collezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci la prima stringa: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Inserisci la seconda stringa: ");
+            string s2 = Console.ReadLine();
+            bool uguaglianza = MetodiCollezioni.Uguali(s1, s2);
+            Console.Write("Le stringhe sono uguali? " + uguaglianza);
+
+            Console.WriteLine();
+
+            Console.Write("Inserisci una stringa: ");
+            string s3 = Console.ReadLine();
+            bool maiuscole = MetodiCollezioni.Maiuscola(s3);
+            Console.Write("La stringa contiene lettere maiuscole? " + maiuscole);
+        }
+    }
+}
---
+++ Libreria/MetodiCollezioni.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Libreria
+{
+    public class MetodiCollezioni
+    {
+        public static bool Uguali(string s1, string s2)
+        {
+            if (s1 == s2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool Maiuscola(string s3)
+        {
+            int maiuscole = 0;
+            for (int i = 0; i < s3.Length; i++)
+            {
+                if (Char.IsUpper(s3[i]) == true)
+                    maiuscole++;
+            }
+            if (maiuscole > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
